ce65dbda6501d59560439c5f2aa44659fbfe8dbf
Mathieu GONON <mgonon@e3r1p12.42.fr>
2017-11-01 00:32:01 +0100

    signin

--- signin.php
+++ signin.php
@@ -4,26 +4,20 @@
 <div class="box-sign-in">
     <form action="#" method="post" class="sign-in">
         <div class="form-group">
-            <input type="text" autofocus="autofocus" required="required" placeholder="Username" name="user[username]" id="user_username">
-        </div>
-        <div class="form-group">
-            <input type="text" required="required" placeholder="Email" name="user[email]" id="user_email">
+            <input type="text" autofocus="autofocus" required="required" placeholder="Username or email" name="user[login]" id="user_username">
         </div>
         <div class="form-group">
             <input type="password" required="required" placeholder="Password" name="user[password]" id="user_password">
         </div>
         <div class="form-group">
-            <input type="password" required="required" placeholder="Password confirm" name="user[password_confirm]" id="user_password_confirm">
+            <input type="submit" name="commit" value="Sign in">
         </div>
-        <div class="form-group">
-            <input type="submit" name="commit" value="Register">
+        <div class="box-center text-big">
+            Forgot your password? <a href="#">Reset it</a>
         </div>
     </form>
 </div>
 
-<div class="box-center text-big">
-    Already have an account? <a href="#">Sign in</a>
-</div>
 
 <div class="fake-row"></div>
 <div class="fake-row"></div>
