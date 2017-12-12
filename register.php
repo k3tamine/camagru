06be7fcba94b53af7a35f3366ae49f16ab8033c6
Mathieu GONON <mgonon@student.42.fr>
2017-12-12 14:10:29 +0100

    some verif

--- register.php
+++ register.php
@@ -1,26 +1,52 @@
-<?php require_once('Views/header.php')
+<?php 
+require_once('Views/header.php');
 
-// if(!empty($_POST))
-// {
-//     $errors = array();
-//     if(empty($_POST['user_username']) || !preg_match('/^[a-zA-Z0-9_]+$/'), $_POST['user_username'])
-//     {
-//         $errors['user_username'] = "Pseudo invalide (a-zA-Z0-9_ autorisé)";
-//     }
-//     else if(empty($_POST['user_email']) || !filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)){
-//         $errors['user_email'] = "Email invalide (FILTER_VALIDATE_EMAIL)";
-//     }
-//     else if(empty($_POST['user_password']) || $_POST['user_password'] != $_POST['user_password_confirm'])
-//     {
-//         $errors['user_password'] = "Password incorrect";
-//     }
-// }
+// POUR INSCRIPTION
+if (!empty($_POST))
+{
+    $errors = array();
+    if (empty($_POST['user_username']) || !preg_match('/^[a-zA-Z0-9_]+$/', $_POST['user_username'])){
+        $errors['user_username'] = "Invalid username ('a-zA-Z0-9_' allowed)";
+    } else {
+        //CHECK IF USERNAME ALREADY EXIST IN DB
+    }
+    if (empty($_POST['user_email']) || !filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)){
+        $errors['user_email'] = "Invalid email (FILTER_VALIDATE_EMAIL)";
+    } else {
+        //CHECK IF USER_EMAIL ALREADY EXIST IN DB        
+    }
+    if (empty($_POST['user_password']) || $_POST['user_password'] != $_POST['user_password_confirm']){
+        $errors['user_password'] = "Incorrect password";
+    }
+    if (empty($errors)){
+        //ADD USER TO DB
+    }
+}
+
+// POUR CONNEXION
+if (!empty($_POST))
+{
+    $errors = array();
+    if (!empty($_POST['user_login']) && !empty($_POST['user_password'])){
+        //CHECK IF LOGIN/PASSWORD MATCH THEN CONNECT
+    } else {
+        $errors['login_error'] = "Your login or password is incorrect"
+    }
+}
 
 ?>
 <div class="fake-row"></div>
 
 <h2>Please log in or register</h2>
 
+<?php if (!empty($erros)) ?>
+<div class="txt-error">
+    <ul>
+        <?php foreach($errors as $error){
+            echo "<li>${error}</li>";
+        } ?>
+    </ul>
+</div>
 
 <img src="../assets/pics/fleur.png" alt="tamere" class="flower" id="flower">
 <img src="../assets/pics/fleur2.png" alt="tamere" class="flower" id="flower2">
